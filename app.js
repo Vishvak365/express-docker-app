d7c78e913ce93eb3e680e36786918c8e668bd6d8
Vishvak Seenichamy <Vishvak365@gmail.com>
2021-07-21 21:07:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,4 @@ const app = express()
 app.get('/', async (req, res, next) => {
   res.status(200).send('Hello World!')
 })
-app.get('/test', async (req,res,next) => {
-  res.status(202).json('message': 'this is a test message deployed')
-})
 app.listen(3000, () => console.log('Server is running on port 3000'))
